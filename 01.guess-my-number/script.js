3c8b866e2fc207debe74507033f28822ac10a0f4
Rafał Prokopiak <rafal@prokopiak.pl>
2021-10-14 11:22:50 +0200

    task finished

--- 01.guess-my-number/script.js
+++ 01.guess-my-number/script.js
@@ -1,25 +1,33 @@
 'use strict';
-
+//declarations
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-
 let score = 20;
 let highScore = 0;
-document.querySelector('.score').textContent = score;
+displayScore(score);
 
-const displayMessage = function (str) {
-  document.querySelector('.message').textContent = str;
+//assistant functions
+const displayMessage = function (message) {
+  document.querySelection('.message').textContent = message;
+};
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
 };
 
+//event listeners
+/////////AGAIN
 document.querySelector('.again').addEventListener('click', function () {
-  document.querySelector('.score').textContent = 20;
+  displayScore(20);
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('.guess').value = '';
   document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+  displayMessage('Start guessing...');
 
   document.querySelector('body').style.backgroundColor = '#222';
 });
 
+/////////CHECK
 document.querySelector('.check').addEventListener('click', function () {
   const guess = +document.querySelector('.guess').value;
 
@@ -40,25 +48,14 @@ document.querySelector('.check').addEventListener('click', function () {
     }
 
     //too high number
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      displayMessage('Too high!');
+      displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
       score--;
-      document.querySelector('.score').textContent = score;
+      displayScore(score);
     } else {
       displayMessage('You lost the game looser');
-      document.querySelector('.score').textContent = 0;
-    }
-
-    //too low number
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      displayMessage('Too low!');
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game!';
-      document.querySelector('.score').textContent = 0;
+      displayScore(0);
     }
   }
 });
---
+++ 02.modal-components/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const hideModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', showModal);
+}
+
+btnCloseModal.addEventListener('click', hideModal);
+overlay.addEventListener('click', hideModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) hideModal();
+});
