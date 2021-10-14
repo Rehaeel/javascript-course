070657db8cfad0e8f055deb68923e727360b3c26
Rafał Prokopiak <rafal@prokopiak.pl>
2021-10-14 14:24:53 +0200

    refactor pig-game

--- 03.pig-game/script.js
+++ 03.pig-game/script.js
@@ -13,31 +13,29 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-let scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
-
-// Helper functions/declarations
-const newGame = function () {
-  document.querySelector('.player--winner').classList.remove('player--winner');
-  !diceEl.classList.contains('hidden') ? diceEl.classList.add('hidden') : '';
-  scores = [0, 0];
-  currentScore = 0;
-  activePlayer = 0;
-  player0El.classList.add('player--active');
-  playing = true;
+const init = function () {
   score0El.textContent = 0;
   score1El.textContent = 0;
   current0El.textContent = 0;
   current1El.textContent = 0;
-};
+  diceEl.classList.add('hidden');
 
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  !diceEl.classList.contains('hidden') ? diceEl.classList.add('hidden') : '';
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+};
+init();
+
+// Helper functions/declarations
 const nextPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
@@ -95,9 +93,9 @@ btnHold.addEventListener('click', holdPoints);
 
 // NewGame
 document.addEventListener('keydown', function (e) {
-  if (e.code === 'Delete') newGame();
+  if (e.code === 'Delete') init();
 });
 btnNew.addEventListener('click', function () {
   diceEl.focus();
-  newGame();
+  init();
 });
